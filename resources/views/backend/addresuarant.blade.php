a8bbd8343e1c0f21791e299ec9c33229f518f74b
lz <lz@lz-Lenovo-ideapad-320-15ISK>
2017-11-28 13:33:45 +0530

    Add Resturant

---
+++ resources/views/backend/addresuarant.blade.php
@@ -0,0 +1,45 @@
+@extends('backend.layouts.app')
+@section('content') 
+
+{{ Form::open(['route' => 'admin.dashboard', 'files' => true]) }} 
+{{ csrf_field() }}
+
+
+<div class="form-group">
+    <label for="name">Resturant Name </label>
+    <input type="text" class="form-control" id="name" name="name">
+</div>
+
+<div class="form-group">
+    <label for="name">Longitude </label>
+    <input type="text" class="form-control" id="longitude" name="longitude">
+</div>
+
+<div class="form-group">
+    <label for="name">Latitude </label>
+    <input type="text" class="form-control" id="latitude" name="latitude">
+</div>
+
+<div class="form-group">
+    <label for="name">Address </label>
+    <input type="text" class="form-control" id="address" name="address">
+</div>
+
+<div class="form-group">
+    <label for="name">Contact Person </label>
+    <input type="text" class="form-control" id="contact" name="contact">
+</div>
+
+<div class="form-group">
+    <label for="name">Delivery Radius </label>
+    <input type="text" class="form-control" id="contact" name="contact">
+</div>
+
+<div class="form-group"> 
+    <a href="{{ route('admin.dashboard')}}">
+        <button class="btn btn-primary">Add</button>
+    </a> 
+</div>
+
+
+@endsection
