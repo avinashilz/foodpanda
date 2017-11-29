40ab72cd473227accded3aa75abae0b6e23344b8
simrata <simrataarora.luminoguru@gmail.com>
2017-11-29 18:07:37 +0530

    select changes

--- resources/views/backend/additemform.blade.php
+++ resources/views/backend/additemform.blade.php
@@ -1,25 +1,21 @@
 @extends('backend.layouts.app')
 @section('content')
-@foreach (
-$categories as $category
-)
 
-{{$category->categories}}
-@endforeach
 {{ Form::open(['route' => 'admin.additem', 'files' => true]) }}
 {{ csrf_field() }}
 
 <div class="row"> <div class="col-sm-6">
+        {{ Form::label('select','Select Restaurant') }}
+    </div>
+    <div class="col-sm-6"> 
+        {{Form::select('restaurant', $restaurant, null, ['placeholder' => 'Choose Restaurant...'])}}
+    </div>
+</div>
+<div class="row"> <div class="col-sm-6">
         {{ Form::label('select','Select Category') }}
     </div>
     <div class="col-sm-6"> 
-        <!--        {{Form::select('size', ['snacks' => 'Snacks', 'maincourse' => 'Main Course','breads'=>'Bread','deserts'=>'Deserts'], null, ['placeholder' => 'Choose an Item...'])}}-->
-        <!--{{Form::select('categories', [$category->categories], null, ['placeholder' => 'Choose an Item...'])}}-->
-        <select class="form-control" name="categories">
-            @foreach($categories as $category)
-            <option value="{{$category->id}}">{{$category}}</option>
-            @endforeach
-        </select>
+        {{Form::select('categories', $categories, null, ['placeholder' => 'Choose an Item...'])}}
     </div>
 </div>
 <div class="row"> <div class="col-sm-6">
