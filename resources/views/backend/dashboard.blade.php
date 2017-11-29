aaa7cc7d0b82eb6e98ea4a6b9834dffb9dd6804c
simrata <simrataarora.luminoguru@gmail.com>
2017-11-29 12:29:32 +0530

    dashboard set

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -6,13 +6,10 @@
         <small>{{ trans('strings.backend.dashboard.title') }}</small>
     </h1>
 @endsection
-
-
 @section('content')
     <div class="row"> <div class="col-sm-6"> 
         <a href="{{ route('admin.addrestaurantform')}}">
             {{Form::submit('Restaurant',['class' => 'btn'])}}</a>
     </div>
 </div>
-
 @endsection
