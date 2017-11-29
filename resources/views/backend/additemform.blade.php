2bf582943cfc88a95268595724451de8d79c1289
simrata <simrataarora.luminoguru@gmail.com>
2017-11-29 17:33:21 +0530

    category

--- resources/views/backend/additemform.blade.php
+++ resources/views/backend/additemform.blade.php
@@ -1,5 +1,11 @@
 @extends('backend.layouts.app')
-@section('content') 
+@section('content')
+@foreach (
+$categories as $category
+)
+
+{{$category->categories}}
+@endforeach
 {{ Form::open(['route' => 'admin.additem', 'files' => true]) }}
 {{ csrf_field() }}
 
@@ -7,7 +13,13 @@
         {{ Form::label('select','Select Category') }}
     </div>
     <div class="col-sm-6"> 
-        {{Form::select('size', ['snacks' => 'Snacks', 'maincourse' => 'Main Course','breads'=>'Bread','deserts'=>'Deserts'], null, ['placeholder' => 'Choose an Item...'])}}
+        <!--        {{Form::select('size', ['snacks' => 'Snacks', 'maincourse' => 'Main Course','breads'=>'Bread','deserts'=>'Deserts'], null, ['placeholder' => 'Choose an Item...'])}}-->
+        <!--{{Form::select('categories', [$category->categories], null, ['placeholder' => 'Choose an Item...'])}}-->
+        <select class="form-control" name="categories">
+            @foreach($categories as $category)
+            <option value="{{$category->id}}">{{$category}}</option>
+            @endforeach
+        </select>
     </div>
 </div>
 <div class="row"> <div class="col-sm-6">
