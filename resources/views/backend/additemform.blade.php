ebf088d6c55c73f00b6aba9495317e3b9805b2be
simrata <simrataarora.luminoguru@gmail.com>
2017-11-29 13:15:25 +0530

    add item

--- resources/views/backend/additemform.blade.php
+++ resources/views/backend/additemform.blade.php
@@ -1,1 +1,29 @@
+@extends('backend.layouts.app')
+@section('content') 
+{{ Form::open(['route' => 'admin.additem', 'files' => true]) }}
+{{ csrf_field() }}
 
+<div class="row"> <div class="col-sm-6">
+        {{ Form::label('select','Select Category') }}
+    </div>
+    <div class="col-sm-6"> 
+        {{Form::select('size', ['L' => 'Large', 'S' => 'Small'], null, ['placeholder' => 'Choose an Item...'])}}
+    </div>
+</div>
+<div class="row"> <div class="col-sm-6">
+        {{ Form::label('name','Item Name') }}
+    </div>
+    <div class="col-sm-6"> 
+        {{Form::text('name','',['class'=>'textfield'])}}
+    </div>
+</div>
+
+<div class="row"> <div class="col-sm-6">
+        {{ Form::label('price','Item Price') }}
+    </div>
+    <div class="col-sm-6"> 
+        {{Form::number('price','',['class'=>'textfield'])}}
+    </div>
+</div>
+
+@endsection
