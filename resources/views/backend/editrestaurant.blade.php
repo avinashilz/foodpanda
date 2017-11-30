9f95cecbcf66abc076966549b70307082f2e58b6
simrata <simrataarora.luminoguru@gmail.com>
2017-11-30 17:11:25 +0530

    edit rest

--- resources/views/backend/editrestaurant.blade.php
+++ resources/views/backend/editrestaurant.blade.php
@@ -1,19 +1,10 @@
 @extends('backend.layouts.app')
 @section('content')
 
-
-
-
-{{Form::model($restaurant, ['route' => ['admin.restaurants.edit', $restaurant]])}}
-{{ csrf_field() }}
-
 <div class="row"> 
     <a href="{{ route('admin.restaurants.index')}}">
         {{Form::button('Back',['class' => 'btn btn-warning'])}}
     </a> 
 </div>
 
-
-
-{{Form::close}}
 @endsection
