9cfeb2c07c9c18bc48519aedf66212cf665ff541
simrata <simrataarora.luminoguru@gmail.com>
2017-11-30 16:13:30 +0530

    del

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -18,15 +18,23 @@
     @foreach($restaurant as $restro)
     <div class="col-sm-6 display"> 
         <h3> Restaurant Name : <b> {{$restro->name}}<b> </h3> <br>
-        <h4> Address : {{$restro->address}} </h5> <br>
-        <h4> Contact Number : {{$restro->phone}}</h4><br>
-        <h4> Contact Person : {{$restro->contact_person}} </h4> <br>
-        <img src="/uploads/{{$restro->image}}" height="150px" width="250px" /> <br> 
-        <a class="btn btn-info" href="{{ route('admin.restaurants.edit',['id'=> $restro->id]) }}">Edit</a>
-        <a class="btn btn-danger" href="{{ route('admin.restaurants.destroy',['id'=> $restro->id]) }}">Delete</a>
-    </div>
-    <br>
-    @endforeach
+                    <h4> Address : {{$restro->address}} </h5> <br>
+                        <h4> Contact Number : {{$restro->phone}}</h4><br>
+                        <h4> Contact Person : {{$restro->contact_person}} </h4> <br>
+                        <img src="/uploads/{{$restro->image}}" height="150px" width="250px" /> <br> 
+                        <a class="btn btn-info" href="{{ route('admin.restaurants.edit',['id'=> $restro->id]) }}">Edit Restaurant</a>
+                        <a class="btn btn-info" href="{{ route('admin.edititemform',['id'=> $restro->id]) }}">Edit Item</a>
+                        {{ Form::open([
+                            'route' => ['admin.restaurants.destroy', $restro->id],
+                            'method' => 'delete'
+                        ]) }}
+                        {{ csrf_field() }}
+                        {{ Form::submit('Delete',['class' => 'btn btn-danger'])}}
+                        {{ Form::close() }}
 
-</div>
-@endsection
+                        </div>
+                        <br>
+                        @endforeach
+
+                        </div>
+                        @endsection
