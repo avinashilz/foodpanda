86c3637a9f5e89aac94c8de187ef82516acef8a2
simrata <simrataarora.luminoguru@gmail.com>
2017-11-30 19:02:57 +0530

    info

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -7,8 +7,8 @@
 
 <div class="row" style="text-align: center;"> 
     
-        <a class="btn btn-primary" href="{{ route('admin.restaurants.create')}}">Restaurant</a>
-        <a class="btn btn-primary" href="{{ route('admin.additemform')}}">Item</a>
+        <a class="btn btn-info" href="{{ route('admin.restaurants.create')}}">Restaurant</a>
+        <a class="btn btn-info" href="{{ route('admin.additemform')}}">Item</a>
     
 </div>
 <div class="row">
@@ -18,7 +18,7 @@
                     <h4> Address : {{$restro->address}} </h5> <br>
                         <h4> Contact Number : {{$restro->phone}}</h4><br>
                         <h4> Contact Person : {{$restro->contact_person}} </h4> <br>
-                        <img src="/uploads/{{$restro->image}}" height="150px" width="250px" /> <br> 
+                        <img src="/uploads/{{$restro->image}}" height="150px" width="280px" /> <br> 
                         <div class=""col-sm-4 align><a class="btn btn-warning" href="{{ route('admin.restaurants.edit',['id'=> $restro->id]) }}">Edit Restaurant</a></div>
                         <div class=""col-sm-4 align> <a class="btn btn-warning" href="{{ route('admin.edititemform',['id'=> $restro->id]) }}">Edit Item</a></div>
                         <div class=""col-sm-4 align> {{ Form::open([
