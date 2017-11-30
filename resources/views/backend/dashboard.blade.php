c455444a01095999281d8760a1a5c0c811c8d7cb
simrata <simrataarora.luminoguru@gmail.com>
2017-11-30 18:54:30 +0530

    dash

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -19,8 +19,8 @@
                         <h4> Contact Number : {{$restro->phone}}</h4><br>
                         <h4> Contact Person : {{$restro->contact_person}} </h4> <br>
                         <img src="/uploads/{{$restro->image}}" height="150px" width="250px" /> <br> 
-                        <div class=""col-sm-4 align><a class="btn btn-info edit" href="{{ route('admin.restaurants.edit',['id'=> $restro->id]) }}">Edit Restaurant</a></div>
-                        <div class=""col-sm-4 align> <a class="btn btn-info edit" href="{{ route('admin.edititemform',['id'=> $restro->id]) }}">Edit Item</a></div>
+                        <div class=""col-sm-4 align><a class="btn btn-warning" href="{{ route('admin.restaurants.edit',['id'=> $restro->id]) }}">Edit Restaurant</a></div>
+                        <div class=""col-sm-4 align> <a class="btn btn-warning" href="{{ route('admin.edititemform',['id'=> $restro->id]) }}">Edit Item</a></div>
                         <div class=""col-sm-4 align> {{ Form::open([
                             'route' => ['admin.restaurants.destroy', $restro->id],
                             'method' => 'delete'
