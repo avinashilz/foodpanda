ed52bd98199237fb72d3fa7bafbcfc5938e1e260
simrata <simrataarora.luminoguru@gmail.com>
2017-11-30 12:24:46 +0530

    data displayed

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -7,6 +7,7 @@
 </h1>
 @endsection
 @section('content')
+
 <div class="row"> 
     <div class="col-sm-6"> 
         <a href="{{ route('admin.addrestaurantform')}}">
@@ -18,17 +19,23 @@
     </div>
 </div>
 <div class="row">
+    @foreach($restaurant as $restro)
     <div class="col-sm-6"> 
-         
-    </div>
-    <div class="col-sm-6">
-         <a href="{{ route('admin.editrestaurantform')}}">
-            {{Form::button('Edit',['class' => 'btn'])}}
+        {{$restro->name}} <br>
+        {{$restro->address}}<br>
+        {{$restro->phone}}<br>
+        {{$restro->contact_person}}<br>
+        {{$restro->image}}<br>
+        <img src="/uploads/{{$restro->image}}" height="150px" width="250px" /> <br> 
+        <a href="{{ route('admin.dashboard')}}">
+            {{Form::button('Edit',['class' => 'btn'])}} 
         </a>
         <a href="{{ route('admin.dashboard')}}">
             {{Form::button('Delete',['class' => 'btn'])}}
         </a>
     </div>
+
+    @endforeach
 
 </div>
 @endsection
