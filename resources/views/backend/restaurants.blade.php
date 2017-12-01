eef96ba666707280d870ada1817af2fa695932f2
simrata <simrataarora.luminoguru@gmail.com>
2017-12-01 11:02:40 +0530

    288

--- resources/views/backend/restaurants.blade.php
+++ resources/views/backend/restaurants.blade.php
@@ -23,7 +23,7 @@
             <h4> Address : {{$restro->address}} </h5> <br>
                 <h4> Contact Number : {{$restro->phone}}</h4><br>
                 <h4> Contact Person : {{$restro->contact_person}} </h4> <br>
-                <img src="/uploads/{{$restro->image}}" height="150px" width="280px" /> <br> 
+                <img src="/uploads/{{$restro->image}}" height="150px" width="288px" /> <br> 
                 <div class=""col-sm-4 align><a class="btn btn-warning" href="{{ route('admin.restaurants.edit',['id'=> $restro->id]) }}">Edit Restaurant</a></div>
                 <div class=""col-sm-4 align> <a class="btn btn-warning" href="{{ route('admin.edititemform',['id'=> $restro->id]) }}">Edit Item</a></div>
                 <div class=""col-sm-4 align> {{ Form::open([
