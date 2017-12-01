cd08d431984facc35a815a4f59c41d3c9c0d913b
simrata <simrataarora.luminoguru@gmail.com>
2017-12-01 10:14:57 +0530

    id hidden

--- resources/views/backend/additemform.blade.php
+++ resources/views/backend/additemform.blade.php
@@ -1,31 +1,37 @@
 @extends('backend.layouts.app')
 @section('content')
 
-{{ Form::open(['route' => 'admin.additem', 'files' => true]) }}
-{{ csrf_field() }}
 <div class="row"> 
     <a href="{{ route('admin.restaurants.index')}}">
         {{Form::button('Back',['class' => 'btn btn-warning'])}}
     </a> 
 </div>
-<div class="row"> <div class="col-sm-6">
+{{ Form::open(['route' => 'admin.additem', 'files' => true]) }}
+{{ csrf_field() }}
+<div class="row"> 
+    @if(isset($restaurantselected['name'])) 
+
+    <div class="col-sm-6">
+
+        {{ Form::label('select','Restaurant Name') }}
+    </div>
+    <div class="col-sm-6">
+        {{Form::hidden('id',$restaurantselected->id)}}
+        {{$restaurantselected->name}}
+    </div>
+
+    @else 
+    <div class="col-sm-6"> 
         {{ Form::label('select','Select Restaurant') }}
     </div>
     <div class="col-sm-6"> 
-
-
-        @if(isset($restaurantselected['name'])) 
-
-        {{Form::hidden('id',$restaurantselected->id)}}
-        {{$restaurantselected->name}}
-
-        @else 
-
         {{Form::select('restaurant',$restaurant, null, ['placeholder' => 'Choose Restaurant...'])}}
-        @endif
     </div>
+    @endif
 </div>
-<div class="row"> <div class="col-sm-6">
+
+<div class="row">
+    <div class="col-sm-6">
         {{ Form::label('select','Select Category') }}
     </div>
     <div class="col-sm-6"> 
