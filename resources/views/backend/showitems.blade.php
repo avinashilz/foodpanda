f73c800da0327243683a29d31643e720191f1aeb
simrata <simrataarora.luminoguru@gmail.com>
2017-12-01 14:37:00 +0530

    changes

--- resources/views/backend/showitems.blade.php
+++ resources/views/backend/showitems.blade.php
@@ -15,16 +15,16 @@
     <div class="row">
         <h2> <b>Category: {{$category->categories}} </b></h2> <br>
         @foreach($category->items as $item)
-        <div class="col-sm-6 display"> 
+        <div class="col-sm-3 display"> 
             <h3> Item Name :{{$item->name}}  </h3> <br>
             <h4> Price :{{$item->price}} </h4><br>
-            <div class=""col-sm-4 align> <a href="{{ route('admin.edititemform',['id'=> $item->id]) }}"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a></div>
-            <div class=""col-sm-4 align> {{ Form::open([
+            <div class=""col-sm-4 align> <a href="{{ route('admin.edititemform',['id'=> $item->id]) }}"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>            {{ Form::open([
                             'route' => ['admin.restaurants.destroy', $item->id],
                             'method' => 'delete'
                         ]) }}
+               
                 {{ csrf_field() }}
-                {{ Form::submit('Delete',['class' => 'btn btn-danger edit'])}}
+                {{ Form::submit('X',['class' => 'btn btn-danger edit'])}}
                 {{ Form::close() }}
             </div>
         </div>
