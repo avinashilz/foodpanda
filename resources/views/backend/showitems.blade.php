ab1a41e65e90ef20493ca4a25a110296bbfd34b5
simrata <simrataarora.luminoguru@gmail.com>
2017-12-01 13:29:17 +0530

    changes

--- resources/views/backend/showitems.blade.php
+++ resources/views/backend/showitems.blade.php
@@ -9,20 +9,29 @@
         <i class="fa fa-hand-o-left" aria-hidden="true"></i>
     </a> 
 </div>
+
 <div>
+    @foreach($categories as $category)
     <div class="row">
-        <h2> <b> </b></h2> <br>
-        @foreach($item as $items)
-
+        <h2> <b>Category: {{$category->categories}} </b></h2> <br>
+        @foreach($category->items as $item)
         <div class="col-sm-6 display"> 
-
-            <h3> Item Name :{{$items->name}}  </h3> <br>
-            <h4> Price :{{$items->price}} </h4><br>
-            <h4> Category: {{$items->category_id}} </h4> <br> 
-
+            <h3> Item Name :{{$item->name}}  </h3> <br>
+            <h4> Price :{{$item->price}} </h4><br>
+            <div class=""col-sm-4 align> <a class="btn btn-warning" href="{{ route('admin.edititemform',['id'=> $item->id]) }}">Edit Item</a></div>
+            <div class=""col-sm-4 align> {{ Form::open([
+                            'route' => ['admin.restaurants.destroy', $item->id],
+                            'method' => 'delete'
+                        ]) }}
+                {{ csrf_field() }}
+                {{ Form::submit('Delete',['class' => 'btn btn-danger edit'])}}
+                {{ Form::close() }}
+            </div>
         </div>
         @endforeach
+
     </div>
+    @endforeach
 
 </div>
 </div>
