f18439d9b7ee0ab63e43671da0c17ec082338dd6
simrata <simrataarora.luminoguru@gmail.com>
2017-12-01 13:33:29 +0530

    changes

--- resources/views/backend/edititem.blade.php
+++ resources/views/backend/edititem.blade.php
@@ -6,14 +6,24 @@
 @section('content')
 <div class="row"> 
     <a href="{{ route('admin.restaurants.index')}}">
-         <i class="fa fa-hand-o-left" aria-hidden="true"></i>
+        <i class="fa fa-hand-o-left" aria-hidden="true"></i>
     </a> 
 </div>
 
-<div class="row" style="text-align: center">
-
-
-    <h3> Item Name : <b><b> </h3> <br>
-                <h4> Price : ₹ </h5> <br>
-                    </div>
-                    @endsection
+<div class="row">
+    
+    <div class="row"> <div class="col-sm-6">
+            {{ Form::label('name','Item Name') }}
+        </div>
+        <div class="col-sm-6"> 
+            {{Form::text('name','',['class'=>'textfield'])}}
+        </div>
+    </div>
+    <div class="row"> <div class="col-sm-6">
+            {{ Form::label('price','Item Price (in ₹:)') }}
+        </div>
+        <div class="col-sm-6"> 
+            {{Form::number('price','',['class'=>'no-spin'])}}
+        </div>
+    </div>
+    @endsection
