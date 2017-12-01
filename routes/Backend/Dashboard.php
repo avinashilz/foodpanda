43f57fb73a5e47660d0c13b216ef60030c75f9bc
Avinashi <avinashi.luminoguru@gmail.com>
2017-12-01 10:27:50 +0530

    check

--- routes/Backend/Dashboard.php
+++ routes/Backend/Dashboard.php
@@ -3,7 +3,7 @@
 /**
  * All route names are prefixed with 'admin.'.
  */
-//Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+Route::get('dashboard', 'DashboardController@index')->name('dashboard');
 
 Route::resource('restaurants', 'RestaurantsController');
 
