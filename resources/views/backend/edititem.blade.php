89d7b4a2369123beed8d1b668ef728b6a1982284
simrata <simrataarora.luminoguru@gmail.com>
2017-12-01 11:56:40 +0530

    item edit

--- resources/views/backend/edititem.blade.php
+++ resources/views/backend/edititem.blade.php
@@ -1,21 +1,17 @@
 @extends('backend.layouts.app')
 
 @section('page-header')
-<h1>
-    {{ app_name() }}
-    <small>{{ trans('strings.backend.dashboard.title') }}</small>
-</h1>
+
 @endsection
 @section('content')
 
-<div class="row">
-    @foreach($items as $item)
-    <div class="col-sm-6 display"> 
-        <h3> Item Name : <b> {{$item->name}}<b> </h3> <br>
-        <h4> Price : ₹{{$item->price}} </h5> <br>    
-    </div>
-    <br>
-    @endforeach
+<div class="row" style="text-align: center">
+   
+    
+        <h3> Item Name : <b><b> </h3> <br>
+        <h4> Price : ₹ </h5> <br>    
 
+   
 </div>
+
 @endsection
