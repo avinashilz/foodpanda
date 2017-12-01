d07c0d391e434e07a254db8ed7324b82e05019f6
Avinashi <avinashi.luminoguru@gmail.com>
2017-12-01 12:52:57 +0530

    menu data

--- app/Http/Controllers/Backend/ItemsController.php
+++ app/Http/Controllers/Backend/ItemsController.php
@@ -1,65 +1,52 @@
 <?php
 
 namespace App\Http\Controllers\Backend;
-use App\Http\Controllers\Controller;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Restaurant;
 use App\Models\Category;
 use App\Models\Item;
 
+class ItemsController extends Controller {
 
-class ItemsController extends Controller
-{
-    public function index(Request $request)
-    {
-//        dd($request->all());
+    public function index(Request $request) {
+        if (!empty($request->all())) {
 
-        
-        if(!empty($request->all())) {
-            
-        $restaurantselected = Restaurant::where('id', $request->all())->first();
+            $restaurantselected = Restaurant::where('id', $request->all())->first();
         }
-//        dd($restaurantselected->toArray());
         $categories = Category::pluck('categories', 'id');
-//        dd($categories);
-//        foreach($categories as $category){
-//            dump($category);
-//        }
         $restaurant = Restaurant::pluck('name', 'id');
-//        foreach($restaurant as $r) {
-//            dump($r->name);
-//        }
-        return view('backend.additemform', compact('categories', 'restaurant', 'restaurantselected')); 
+
+        return view('backend.additemform', compact('categories', 'restaurant', 'restaurantselected'));
     }
-    public function additem(Request $request, Restaurant $restaurantselected)
-    {
-//        dd($request->toArray());
-//        dd($restaurantselected->toArray());
+
+    public function additem(Request $request, Restaurant $restaurantselected) {
+
         $this->validate($request, [
             'name' => 'required',
             'price' => 'required',
         ]);
-        
+
         $item = new Item;
         $item->name = $request->name;
         $item->price = $request->price;
         $item->resturants_id = $request->restaurant;
         $item->category_id = $request->categories;
-        
+
         $item->save();
-        
-        
+
         return redirect()->route('admin.restaurants.index');
     }
-    
+
     public function edit() {
-        
+
         return view('backend.edititem');
     }
-    
+
     public function update() {
-        
+
         return back();
     }
+
 }
