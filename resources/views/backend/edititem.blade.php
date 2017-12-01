94226d1085be4c64a30632992c7b2294af591345
simrata <simrataarora.luminoguru@gmail.com>
2017-12-01 13:42:51 +0530

    changes

--- resources/views/backend/edititem.blade.php
+++ resources/views/backend/edititem.blade.php
@@ -4,26 +4,43 @@
 
 @endsection
 @section('content')
+
 <div class="row"> 
     <a href="{{ route('admin.restaurants.index')}}">
         <i class="fa fa-hand-o-left" aria-hidden="true"></i>
     </a> 
 </div>
 
-<div class="row">
-    
-    <div class="row"> <div class="col-sm-6">
-            {{ Form::label('name','Item Name') }}
-        </div>
-        <div class="col-sm-6"> 
-            {{Form::text('name','',['class'=>'textfield'])}}
-        </div>
+{{ Form::model($item, ['route' => ['admin.updateitem',$item->id],'files'=>true])}} 
+
+
+{{ csrf_field() }}
+{{ method_field('PUT') }}
+
+
+
+<div class="row"> <div class="col-sm-6">
+        {{ Form::label('name','Item Name') }}
     </div>
-    <div class="row"> <div class="col-sm-6">
-            {{ Form::label('price','Item Price (in ₹:)') }}
-        </div>
-        <div class="col-sm-6"> 
-            {{Form::number('price','',['class'=>'no-spin'])}}
-        </div>
+    <div class="col-sm-6"> 
+        {{Form::text('name',$item->name,['class'=>'textfield'])}}
     </div>
-    @endsection
+</div>
+<div class="row"> <div class="col-sm-6">
+        {{ Form::label('price','Item Price (in ₹:)') }}
+    </div>
+    <div class="col-sm-6"> 
+        {{Form::number('price',$item->price,['class'=>'no-spin'])}}
+    </div>
+</div>
+<div class="row"> <div class="col-sm-6"> </div>
+    <div class="col-sm-6"> 
+
+        {{Form::submit('Update',['class' => 'btn btn-info right'])}}
+
+
+    </div>
+</div>
+{{Form::close()}}
+
+@endsection
