508683d2f6183473cae0c85b6346b35320e7bb5c
simrata <simrataarora.luminoguru@gmail.com>
2017-12-01 15:50:52 +0530

    changes

--- resources/views/backend/showitems.blade.php
+++ resources/views/backend/showitems.blade.php
@@ -12,25 +12,36 @@
 
 <div>
     @foreach($categories as $category)
-    <div class="row">
-        <h2> <b>Category: {{$category->categories}} </b></h2> <br>
-        @foreach($category->items as $item)
-        <div class="col-sm-3 display"> 
-            <h3> Item Name :{{$item->name}}  </h3> <br>
-            <h4> Price :{{$item->price}} </h4><br>
-            <div class=""col-sm-4 align> <a href="{{ route('admin.edititemform',['id'=> $item->id]) }}"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>            {{ Form::open([
+
+    <div class="col-sm-6">
+        <h2> <b> Category:{{$category->categories}} </b></h2> <br>
+       
+       
+            <div class="row">
+                <div class="col-sm-5"><h3> Item Name</h3></div>  <div class="col-sm-2"><h3>Price</h3> </div>
+            </div>
+            @foreach($category->items as $item)
+            <div class="row">
+                <div class="col-sm-5"> <h4>  {{$item->name}}</h4></div> <div class="col-sm-2"><h4> {{$item->price}} </h4> </div>
+                <div class="col-sm-1">
+                    <a href="{{ route('admin.edititemform',['id'=> $item->id]) }}"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>            
+                </div>
+                <div class="col-sm-1">
+                    {{ Form::open([
                             'route' => ['admin.item.destroy', $item->id],
                             'method' => 'delete'
                         ]) }}
-               
-                {{ csrf_field() }}
-                {{ Form::submit('X',['class' => 'btn btn-danger edit'])}}
-                {{ Form::close() }}
-            </div>
-        </div>
-        @endforeach
 
+                    {{ csrf_field() }}
+                    {{ Form::submit('X',['class' => 'btn cross'])}}
+                    {{ Form::close() }}
+                </div>
+            </div>
+             @endforeach
+  
+       
     </div>
+
     @endforeach
 
 </div>
