dc95c9b08d40aa1174684f1e59f14abbee815cb6
simrata <simrataarora.luminoguru@gmail.com>
2017-12-09 16:03:17 +0530

    changes

--- resources/views/frontend/user/show.blade.php
+++ resources/views/frontend/user/show.blade.php
@@ -1,5 +1,32 @@
 @extends('frontend.layouts.app')
 @section('content')
+<div class="vendors-heading-container">
+    <div class="container">
+        <div class="vendors-heading">
+            <h1>
+                Order  from 79 restaurants
+            </h1>
+            <h2>delivering to your door</h2>
+        </div>
+    </div>
+
+    <div class="location-header">
+        <div class="container">
+
+            <span class="location-address-container">
+                <i class="icon icon-location"></i>
+
+                Sector 71
+            </span>
+
+
+            <a href="/" class="btn btn-primary btn-sm btn-alt change-location-link js-change-location-link">Change location</a>
+
+        </div>
+    </div>
+</div>
+<div class="container-overlay container-overal-rlp"></div>
+</div>
 
 <div>
     @foreach($categories as $category)
@@ -26,7 +53,7 @@
             <div class="col-sm-1">{{Form::number('qty','',['min'=>1,'max'=>10])}} </div>
             <div class="col-sm-2" style="text-align:  center;">
                 <a href="">
-                <i id="cart" class="fa  fa-cart-arrow-down" aria-hidden="true"></i>
+                    <i id="cart" class="fa  fa-cart-arrow-down" aria-hidden="true"></i>
                 </a>
             </div>
 
