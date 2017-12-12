e7b7eef55c75913c83f2c69910e4d0e4f0c46a89
simrata <simrataarora.luminoguru@gmail.com>
2017-12-12 12:28:49 +0530

    cart

--- resources/views/frontend/cart.blade.php
+++ resources/views/frontend/cart.blade.php
@@ -1,1 +1,15 @@
-{{dd($detail)}}
+@extends('frontend.layouts.app')
+@section('content')
+</div>
+<div> <h2> Your Order </h2> </div>
+@foreach($detail as $details)
+<div class="row">
+     <div class="col-sm-1"><h4> {{$details['quantity']}} </h4> </div>
+    <div class="col-sm-2"> <h4>  {{$details['name']}}</h4></div> 
+    <div class="col-sm-2"><h4> {{$details['totalprice']}} </h4> </div>
+</div>
+
+ @endforeach
+ 
+
+@endsection
