8071ce5fea89650fe769ede3a6ac69bdeb855cda
Avinashi <avinashi.luminoguru@gmail.com>
2017-12-13 14:51:40 +0530

    controller

---
+++ app/Http/Controllers/Frontend/User/FileEntryController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use App\Models\Fileentry;
+
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
+use Illuminate\Http\Response;
+
+
+class FileEntryController extends Controller
+{
+    public function get(Request $request, $filename) {
+//        dd($request->ajax());
+        $entry = Fileentry::where('filename', '=', $filename)->firstOrFail();
+		$file = Storage::disk('local')->get($entry->filename);
+		return response($file, 200)->header('Content-Type', $entry->mime);
+    }
+}
